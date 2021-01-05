0fe4ac9e064b72d80f156c47a0117e3accb28f25
Eugene Mwangi <mwangieugen80@gmail.com>
2021-01-05 17:40:46 +0300

    adds code to ensure valid lcd values are displayed

--- src/main.cpp
+++ src/main.cpp
@@ -26,7 +26,7 @@
 
 #include <LiquidCrystal_I2C.h>
 
-#define SCAN_TIME 10   // seconds
+#define SCAN_TIME 10  // seconds
 #define SLEEP_TIME 60 //seconds
 
 boolean METRIC = true; //Set true for metric system; false for imperial
@@ -50,6 +50,9 @@ String convertFloatToString(float f);
 float CelciusToFahrenheit(float Celsius);
 
 LiquidCrystal_I2C lcd(0x27, 20, 4);
+void display();
+RTC_DATA_ATTR float lcd_temp = -100;
+RTC_DATA_ATTR float lcd_hum = -100;
 
 class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks
 {
@@ -121,11 +124,13 @@ class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks
         {
           current_temperature = CelciusToFahrenheit((float)value / 10);
         }
+        (current_temperature == -100) ?: (lcd_temp = current_temperature);
         Serial.printf("TEMPERATURE_EVENT: %s, %d\n", charValue, value);
         sprintf(charValue, "%02X%02X", cServiceData[17], cServiceData[16]);
         value2 = strtol(charValue, 0, 16);
         current_humidity = (float)value2 / 10;
         Serial.printf("HUMIDITY_EVENT: %s, %d\n", charValue, value2);
+        (current_humidity == -100) ?: (lcd_hum = current_humidity);
         break;
       }
     }
@@ -149,6 +154,7 @@ void loop()
 {
 
   char printLog[256];
+  display();
   Serial.printf("Start BLE scan for %d seconds...\n", SCAN_TIME);
   // BLEScan *pBLEScan = BLEDevice::getScan(); //create new scan
   // pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
@@ -157,25 +163,19 @@ void loop()
   int count = foundDevices.getCount();
   printf("Found device count : %d\n", count);
   Serial.printf("temperature: %.2f humidity: %.2f\n", current_temperature, current_humidity);
-  //Print the temperature on the lcd
-  lcd.setCursor(0,0);
-  lcd.print("Temperature: ");
-  lcd.print(current_temperature);
-  lcd.print("C");
-  //print the humidity on the lcd
-  lcd.setCursor(0,2);
-  lcd.print("Humidity: ");
-  lcd.print(current_humidity);
-  lcd.print("%");
 
   // pBLEScan->clearResults();
 
   delay(100);
 
 #if SLEEP_TIME > 0
-  esp_sleep_enable_timer_wakeup(SLEEP_TIME * 1000000); // translate second to micro second
-  Serial.printf("Enter deep sleep for %d seconds...\n", (SLEEP_TIME));
-  esp_deep_sleep_start();
+  if (current_temperature != -100 && current_humidity != -100)
+  {
+    display();
+    esp_sleep_enable_timer_wakeup(SLEEP_TIME * 1000000); // translate second to micro second
+    Serial.printf("Enter deep sleep for %d seconds...\n", (SLEEP_TIME));
+    esp_deep_sleep_start();
+  }
 
 #endif
 }
@@ -201,4 +201,21 @@ float CelciusToFahrenheit(float Celsius)
   float Fahrenheit = 0;
   Fahrenheit = Celsius * 9 / 5 + 32;
   return Fahrenheit;
+}
+/**
+ * @brief Displays the temperature and humidity on the lcd
+*/
+void display()
+{
+  lcd.clear();
+  //Print the temperature on the lcd
+  lcd.setCursor(0, 0);
+  lcd.print("Temperature: ");
+  lcd.print(lcd_temp);
+  lcd.print("C");
+  //print the humidity on the lcd
+  lcd.setCursor(0, 2);
+  lcd.print("Humidity: ");
+  lcd.print(lcd_hum);
+  lcd.print("%");
 }
