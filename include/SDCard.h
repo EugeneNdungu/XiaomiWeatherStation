8430b5fa0779189e1bf1e2d6c1adb6bb89fc429f
EugeneNdungu <mwangieugene80@gmail.com>
2021-04-11 08:20:37 +0300

    Includes functions to write and append to the sd card

--- include/SDCard.h
+++ include/SDCard.h
@@ -4,3 +4,55 @@
  * The header file contains all functions definitions pertaining to the 
  * sd card functionality of the program
  */
+
+#ifndef _SDCARD_
+#define _SDCARD_
+
+#include "Arduino.h"
+#include "FS.h"
+#include "SD.h"
+#include "SPI.h"
+
+void writeFile(fs::FS &fs, const char *path, const char *message)
+{
+    Serial.printf("Writing file: %s\n", path);
+
+    File file = fs.open(path, FILE_WRITE);
+    if (!file)
+    {
+        Serial.println("Failed to open file for writing");
+        return;
+    }
+    if (file.print(message))
+    {
+        Serial.println("File written");
+    }
+    else
+    {
+        Serial.println("Write failed");
+    }
+    file.close();
+}
+
+void appendFile(fs::FS &fs, const char *path, const char *message)
+{
+    Serial.printf("Appending to file: %s\n", path);
+
+    File file = fs.open(path, FILE_APPEND);
+    if (!file)
+    {
+        Serial.println("Failed to open file for appending");
+        return;
+    }
+    if (file.print(message))
+    {
+        Serial.println("Message appended");
+    }
+    else
+    {
+        Serial.println("Append failed");
+    }
+    file.close();
+}
+
+#endif // _SDCARD_
