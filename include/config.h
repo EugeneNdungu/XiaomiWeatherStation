3681ae8af2bf71d967578eef9367b3b3d69c859f
EugeneNdungu <mwangieugene80@gmail.com>
2021-04-17 09:36:03 +0300

    Defines enable headers

---
+++ include/config.h
@@ -0,0 +1,12 @@
+/**
+ * Configuration Header File
+ * 
+ */
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define BLE_ENABLE 0 // If enabled(set to > 0), all ble functionalites are enabled
+#define GSM_ENABLE 0 // If enabled(set to > 0), all gsm functionalites are enabled
+#define SD_ENABLE 1  // If enabled(set to > 0), all sd functionalites are enabled
+#endif               // CONFIG_H
