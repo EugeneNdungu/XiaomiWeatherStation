7c55946cc966c0f8616b75f055bab20e8cd7ceee
EugeneNdungu <mwangieugene80@gmail.com>
2021-04-17 09:59:15 +0300

    Adds readfile function

--- include/SDCard.h
+++ include/SDCard.h
@@ -57,4 +57,23 @@ void appendFile(fs::FS &fs, const char *path, const char *message)
     file.close();
 }
 
+void readFile(fs::FS &fs, const char *path)
+{
+    Serial.printf("Reading file: %s\n", path);
+
+    File file = fs.open(path);
+    if (!file)
+    {
+        Serial.println("Failed to open file for reading");
+        return;
+    }
+
+    Serial.print("Read from file: ");
+    while (file.available())
+    {
+        Serial.write(file.read());
+    }
+    file.close();
+}
+
 #endif // _SDCARD_
